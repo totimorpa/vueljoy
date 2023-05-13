812e500d9303541dee22629019f8f786795927cc
R_Albizures <r.j.albizures@gmail.com>
2023-05-13 19:15:46 +0200

    question update

--- src/components/question/question.js
+++ src/components/question/question.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Typography, Button, Box, Grid, Card } from "@mui/material";
-import LinearProgress from "@mui/material/LinearProgress";
+import LinearProgress, { linearProgressClasses } from "@mui/material/LinearProgress";
+import AirplaneImg from "./Airplane.png";
 
 function Question({ question, answers, onAnswer }) {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
@@ -13,29 +14,27 @@ function Question({ question, answers, onAnswer }) {
   const [progress, setProgress] = useState(0);
 
   useEffect(() => {
-    const timer = setInterval(() => {
+    const interval = setInterval(() => {
       setProgress((oldProgress) => {
-        if (oldProgress === 100) {
-          return 0;
+        if (oldProgress >= 100) {
+          clearInterval(interval);
+          return 100;
         }
-        const diff = Math.random() * 10;
-        return Math.min(oldProgress + diff, 100);
+        return oldProgress + 10;
       });
-    }, 500);
+    }, 1500);
 
-    return () => {
-      clearInterval(timer);
-    };
+    return () => clearInterval(interval);
   }, []);
 
-  const buttonColors = [
-    "#f44336","blue","green","purple"];
+  const buttonColors = ["#f44336", "blue", "green", "purple"];
 
-  const answerHeight = Math.max(
-    ...answers.map((answer) =>
-      typeof answer === "string" ? answer.length : answer.toString().length
-    )
-  ) * 10;
+  const answerHeight =
+    Math.max(
+      ...answers.map((answer) =>
+        typeof answer === "string" ? answer.length : answer.toString().length
+      )
+    ) * 10;
 
   return (
     <Box
@@ -47,13 +46,13 @@ function Question({ question, answers, onAnswer }) {
       m={2}
     >
       <Card sx={{ p: 2 }}>
-        <Typography variant="h4" align="center">
+        <Typography variant="h5" align="center">
           {question}
         </Typography>
         <Box mt={4}>
           <Grid container spacing={2} justifyContent="center">
             {answers.map((answer, index) => (
-              <Grid item xs={6} key={answer}>
+              <Grid item xs={6} sm={6} md={6} lg={6} key={answer}>
                 <Button
                   variant="contained"
                   color="primary"
@@ -63,23 +62,44 @@ function Question({ question, answers, onAnswer }) {
                     height: answerHeight,
                     backgroundColor: buttonColors[index],
                     "&:hover": {
-                      backgroundColor: index === 0 ? "#ffcc00" :
-                        index === 1 ? "#ffcc00" :
-                        index === 2 ? "#ffcc00" :
-                        index === 3 ? "#ffcc00" : null,
+                      backgroundColor:
+                        index === 0
+                          ? "#ffcc00"
+                          : index === 1
+                          ? "#ffcc00"
+                          : index === 2
+                          ? "#ffcc00"
+                          : index === 3
+                          ? "#ffcc00"
+                          : null,
                     },
                   }}
                   onClick={() => handleAnswer(answer)}
                 >
-                  <Typography variant="h5">{answer}</Typography>
+                  <Typography variant="h5" sx={{ overflowWrap: "anywhere" }}>
+                    {answer}
+                  </Typography>
                 </Button>
               </Grid>
             ))}
           </Grid>
         </Box>
       </Card>
-      <Box sx={{ width: "100%", mt: 2 }}>
-        <LinearProgress variant="determinate" value={progress} />
+      <Box sx={{ width: "100%", mt: 2, position: "relative" }}>
+        <LinearProgress
+          classes={{ bar: linearProgressClasses.dashed }}
+          variant="determinate"
+          value={progress}
+        />
+        <Box
+          sx={{
+            position: "absolute",
+            right: `${100 - progress}%`,
+            top: "-30px",
+          }}
+        >
+          <img src={AirplaneImg} alt="Airplane" height="80px" />
+        </Box>
       </Box>
     </Box>
   );
