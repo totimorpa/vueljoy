94187c790161e7ce1d960177fe683dc36e057e12
Toti Moragas <totimorpa@gmail.com>
2023-05-13 14:54:49 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,40 @@ import Login from "./components/login/login.js";
 import LoadingScreen from "./components/loading/loading.js";
 import Question from "./components/question/question.js";
 import Ranking from "./components/ranking/ranking.js";
+import { useState } from "react";
+import SockJS from "sockjs-client";
 
 function App() {
+  const [loadingGame, setLoadingGame] = useState(false);
+  const [gameStarted, setGameStarted] = useState(false);
+  const [gameEnded, setGameEnded] = useState(false);
+
+  var sock = new SockJS("http://192.168.253.20:8080/gs-guide-websocket");
+
+  sock.onopen = function () {
+    console.log("open");
+    sock.send("test");
+  };
+
+  sock.onmessage = function (e) {
+    console.log("message", e.data);
+    sock.close();
+  };
+
+  sock.onclose = function () {
+    console.log("close");
+  };
+
+  function onLogin(name, seat) {
+    console.log("onLogin");
+    console.log(name, seat);
+  }
+
   return (
     <div className="App">
+      <Login onLogin={onLogin}></Login>
       {/* <LoadingScreen prompt="Waiting for game to start" /> */}
-      {/* <Login></Login> */}
+      {/* <LoadingScreen prompt="Wainting for everyone to answer" /> */}
       {/* <Ranking
         players={[
           { id: 123242, name: "Row 1", score: 200 },
@@ -19,7 +47,7 @@ function App() {
           { id: 234567, name: "Row 5", score: 1500 },
         ]}
       ></Ranking> */}
-      <Question
+      {/* <Question
         question={
           "What is the name of the art museum located in Trafalgar Square?"
         }
@@ -30,7 +58,7 @@ function App() {
           "British Museum",
         ]}
         onAnswer={(answer) => console.log(answer)}
-      ></Question>
+      ></Question> */}
     </div>
   );
 }
