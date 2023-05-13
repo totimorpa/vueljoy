6194a9aec1be4b71baa52413f7b754886d14dae0
R_Albizures <r.j.albizures@gmail.com>
2023-05-13 18:40:23 +0200

    question update

--- src/components/question/question.js
+++ src/components/question/question.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Typography, Button, Box, Grid, Card } from "@mui/material";
 import LinearProgress from "@mui/material/LinearProgress";
+import AirplaneIcon from "./AirplaneIcon.png";
 
 function Question({ question, answers, onAnswer }) {
   const [selectedAnswer, setSelectedAnswer] = useState(null);
@@ -46,6 +47,28 @@ function Question({ question, answers, onAnswer }) {
       height="100vh"
       m={2}
     >
+      <Box
+        sx={{
+          position: "relative",
+          display: "inline-flex",
+          alignItems: "center",
+        }}
+      >
+        <Box
+          component="img"
+          src={AirplaneIcon}
+          alt="Airplane"
+          sx={{
+            width: 40,
+            height: 40,
+            position: "absolute",
+            zIndex: 1,
+            left: `${progress}%`,
+            transform: `translateX(-${progress / 2}%)`,
+          }}
+        />
+        <LinearProgress variant="determinate" value={progress} sx={{ width: "100%" }} />
+      </Box>
       <Card sx={{ p: 2 }}>
         <Typography variant="h4" align="center">
           {question}
@@ -62,6 +85,12 @@ function Question({ question, answers, onAnswer }) {
                   sx={{
                     height: answerHeight,
                     backgroundColor: buttonColors[index],
+                    "&:hover": {
+                      backgroundColor: index === 0 ? "#ffcc00" :
+                        index === 1 ? "#ffcc00" :
+                        index === 2 ? "#ffcc00" :
+                        index === 3 ? "#ffcc00" : null,
+                    },
                   }}
                   onClick={() => handleAnswer(answer)}
                 >
@@ -72,9 +101,6 @@ function Question({ question, answers, onAnswer }) {
           </Grid>
         </Box>
       </Card>
-      <Box sx={{ width: "100%", mt: 2 }}>
-        <LinearProgress variant="determinate" value={progress} />
-      </Box>
     </Box>
   );
 }
